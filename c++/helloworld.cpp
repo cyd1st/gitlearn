de0691198e74deac7d460674935d8e9da09bef4a
cyd1st <cyd1997@126.com>
2021-07-28 17:39:03 +0800

    文件头注释

--- c++/helloworld.cpp
+++ c++/helloworld.cpp
@@ -1,8 +1,14 @@
+/*
+ * @Descripttion: hello world
+ * @Author: cyd
+ * @Date: 2021-07-28 11:50:59
+ * @LastEditTime: 2021-07-28 17:38:14
+ */
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout<<"Hello world"<<endl;
+    cout<<"Hello world!"<<endl;
 
     return 0;
 }
