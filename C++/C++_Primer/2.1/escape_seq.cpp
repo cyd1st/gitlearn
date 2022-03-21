0b9eb691805485ccd3b6da7d15850a0647a3dc7a
cyd1st <43594415+cyd1st@users.noreply.github.com>
2022-03-21 17:43:07 +0800

    add C++_primer

---
+++ C++/C++_Primer/2.1/escape_seq.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "\n";
+    cout << "\tHi!\n";
+    cout << "\vHi!\n";
+    cout << "\7";
+    cout << "Hi \x4dO\115!\n";
+
+    return 0;
+}
