5d8b72149ce823647041951c6827394efcfc3a41
cyd1st <43594415+cyd1st@users.noreply.github.com>
2022-05-07 19:55:14 +0800

    c

--- C/io/snprintf.c
+++ C/io/snprintf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#define BUFF_SIZE 11
+#define BUFF_SIZE 10
 int main() 
 {
     unsigned short a = 1234;
-    unsigned int size = 11;
+    unsigned int size = 8;
     char buff[BUFF_SIZE] = {0};
-
-    unsigned long ret = snprintf(buff, size, "%010x", a);
+    unsigned long ret = snprintf(buff, size, "%04x", a);
     //printf("char buff[size] = %c\n", buff[size]);
     //printf("char buff[size+1] = %c\n", buff[size+1]);
     //printf("int buff[size-1] = %d\n", buff[size-1]);
---
+++ C/memory/without_bss.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+double a[1000] = {1, };
+int main()
+{
+    return 0;
+}
