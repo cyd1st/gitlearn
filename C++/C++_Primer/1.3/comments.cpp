a18e773fe6305d9ef966191a1c2aef790fbab24c
cyd1st <43594415+cyd1st@users.noreply.github.com>
2022-07-16 14:17:54 +0800

    change directory name Uinx to Linux.

---
+++ C++/C++_Primer/1.3/comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    cout << "lets input two nums!" << endl;
+    int a = 0, b = 0; // declare two int nums
+    cin >> a >> b; /* te
+    st */ cout << "the two nums is " << a << " and " << b << endl;
+    return 0;
+}
