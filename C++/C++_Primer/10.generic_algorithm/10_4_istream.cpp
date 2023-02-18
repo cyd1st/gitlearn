daa1d79cb39fb5623d1d28205dfd6a26fb19e1cc
cyd1st <cyd1997@126.com>
2023-02-18 16:10:37 +0800

    update

--- C++/C++_Primer/10.generic_algorithm/10_4_istream.cpp
+++ C++/C++_Primer/10.generic_algorithm/10_4_istream.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <iterator>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
+void istream_iterator_acc()
+{
+    istream_iterator<int> input(cin);
+    istream_iterator<int> eof;
+    int i = accumulate(input, eof, 0);
+    cout << i << endl;
+}
 void istream_iterator_test()
 {
-    
+    istream_iterator<int> input(cin);
+    istream_iterator<int> eof;
+    vector<int> vi(input, eof);
+    for (int i : vi)
+        cout << i << " ";
+    cout << endl;
 }
 int main()
 {
+    istream_iterator_acc();
+    istream_iterator_test();
     return 0;
 }
