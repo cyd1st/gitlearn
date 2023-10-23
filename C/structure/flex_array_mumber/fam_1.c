e8b02b88becbeda1f57c67b2d66ddbde19ac88c8
cyd1st <cyd1st@163.com>
2023-10-23 22:45:44 +0800

    auto sync code.

--- C/structure/flex_array_mumber/fam_1.c
+++ C/structure/flex_array_mumber/fam_1.c
@@ -2,6 +2,23 @@
 
 int main()
 {
-    
+    typedef struct {
+        int id;
+        int age;
+        int namelen;
+        char name[];
+    } Stu;
+
+    Stu x;
+    printf("x size: %lu\n", sizeof(x.name)); // x size: 8
+
+    int nameLen = stelen("xiaoming");
+    Stu *y = (Stu *)malloc(sizeof(Stu) + sizeof(char) * (nameLen + 1));
+    y->id = 1;
+    y->age = 18;
+    y->namelen = nameLen;
+    strcpy(y->name, "xiaoming");
+
+    return 0;
 }
 
