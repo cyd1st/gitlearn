472af5dd6695e24c26514b75947e0d35950e9173
cyd1st <43594415+cyd1st@users.noreply.github.com>
2024-03-28 20:49:46 +0800

    add comments for demo1

--- Linux/select/demo1/select_server.c
+++ Linux/select/demo1/select_server.c
@@ -20,6 +20,13 @@
  *   这些工具可以模拟大量的并发连接，并测量服务器处理这些连接的能力。
  *   例如：ab -n 1000 -c 100 http://localhost:8888/ 来模拟 1000 个并发连接。
  *   实际运行这个命令很可能会超时。
+ *
+ * 知识点：
+ *   []C标准库函数：如printf、exit、memset、strlen、read、write、close等。
+ *   []套接字编程：理解什么是套接字（socket），以及如何使用C语言进行套接字编程。包括创建套接字、绑定套接字到服务器地址、监听连接请求、接受新的连接请求、读写数据等。
+ *   []网络编程：理解网络编程的基本概念，如IP地址、端口号、TCP/IP协议等。需要掌握的相关函数和结构体包括sockaddr_in、htons、inet_ntoa等。
+ *   []多客户端处理：理解如何使用select函数来同时处理多个客户端的连接。包括文件描述符集合的操作（如FD_ZERO、FD_SET、FD_ISSET等），以及select函数的使用。
+ *   []错误处理：理解如何使用perror函数来打印错误信息，并使用exit函数来终止程序。
  */
 #include <stdio.h>  // printf  
 #include <stdlib.h> // exit EXIT_FAILURE
@@ -30,8 +37,8 @@
 #include <netinet/in.h> // struct sockaddr_in htons
 #include <arpa/inet.h>  // inet_ntoa
 
-#define MAX_CLIENTS 5
-#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 1024
+#define BUFFER_SIZE 1024 // 很多linux系统的最大文件描述符个数是1024，所以这里设定为1024
 
 // 创建服务器套接字
 int create_server_socket() {
@@ -96,6 +103,7 @@ void handle_new_data(int *fd_array, fd_set *read_fds) {
                 close(fd_array[i]);
                 fd_array[i] = 0;
             } else {
+                // 如果读取到数据，再将其转发到其他所有的客户端
                 for (int j = 0; j < MAX_CLIENTS; j++) {
                     if (fd_array[j] != 0) {
                         write(fd_array[j], buffer, strlen(buffer));
@@ -112,6 +120,7 @@ int main() {
     bind_socket(server_socket, server_addr);
     listen_for_connections(server_socket);
 
+    // 客户端的fd集合
     int fd_array[MAX_CLIENTS] = {0};
     fd_set read_fds;
 
@@ -120,9 +129,14 @@ int main() {
     while (1) {
         FD_ZERO(&read_fds);
         FD_SET(server_socket, &read_fds);
+        /** 
+         * max_fd 表示已经处理的文件描述符的最大值 
+         * select 函数需要这个值去遍历 0-max_fd
+         */
         int max_fd = server_socket;
+        // 初始化文件描述符的集合
         for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (fd_array[i] > 0) {
+            if (fd_array[i] > 0) { // 是否是一个有效的文件描述符
                 FD_SET(fd_array[i], &read_fds);
             }
             if (fd_array[i] > max_fd) {
@@ -130,6 +144,10 @@ int main() {
             }
         }
 
+        /**
+         * 监听其中的可读 fd，其他的不监听。
+         * 第一个参数 nfds 需要加1，是因为序列从0开始，所以max_fd序列大小要+1。
+         */
         if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
             perror("select");
             exit(EXIT_FAILURE);
