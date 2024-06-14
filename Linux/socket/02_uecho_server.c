f0ffe358285f5fad605142ac37b08690142f1911
cyd1st <43594415+cyd1st@users.noreply.github.com>
2024-06-14 10:09:56 +0800

    sync.

---
+++ Linux/socket/02_uecho_server.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFF_SIZE 100
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int serv_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (serv_sock == -1) {
+        error_handling("UDP socket creation error");
+    }
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("bind() error.");
+    }
+
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    size_t str_len = 0;
+    char message[BUFF_SIZE];
+    while (1) {
+        printf("in.\n");
+        str_len = recvfrom(serv_sock, message, BUFF_SIZE, 0,
+            (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        printf("recv message from clnt: %s\n", message);
+        sendto(serv_sock, message, str_len, 0, 
+            (struct sockaddr *)&clnt_addr, clnt_addr_size);
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
