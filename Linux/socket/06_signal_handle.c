3ce52c3db2fac32aacb764427da4a7b25e291cbc
cyd1st <43594415+cyd1st@users.noreply.github.com>
2024-06-17 21:09:50 +0800

    sync.

--- Linux/socket/06_signal_handle.c
+++ Linux/socket/06_signal_handle.c
@@ -7,6 +7,10 @@ void timeout(int sig)
     if (sig == SIGALRM) {
         puts("Time out.");
     }
+    /* alram 函数将会产生一个 SIGALRM 信号，
+     * 结合 main 的 SIGALRM 注册语句，这个信号会
+     * 递归的触发下去。
+     */
     alarm(2);
 }
 
@@ -23,6 +27,7 @@ int main()
     signal(SIGINT, keycontrol);
     alarm(2);
 
+    /* 信号会唤醒 sleep 的进程，所以执行程序快速按几次ctrl+c，就可以迅速结束程序 */
     for (int i = 0; i < 3; i++) {
         puts("wait...");
         sleep(100);
