4460d41d07979c8c70dd207e82f3bd703a602ef7
cyd1st <43594415+cyd1st@users.noreply.github.com>
2024-08-07 10:32:18 +0800

    fix privace email

--- shell/fix-github-private-email.sh
+++ shell/fix-github-private-email.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# 设置新的电子邮件地址
-NEW_EMAIL="43594415+cyd1st@users.noreply.github.com"
+# 检查命令行参数数量
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <OLD_EMAIL> <NEW_EMAIL>"
+    exit 1
+fi
+
+# 从命令行参数中获取旧邮箱和新邮箱
+OLD_EMAIL="$1"
+NEW_EMAIL="$2"
+
+# 获取当前分支名称
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # 获取包含私有电子邮件的所有提交哈希值
-COMMITS=$(git log --format="%H" --grep="cyd1997@126.com")
+COMMITS=$(git log --format="%H" --grep=$OLD_EMAIL)
 
 # 暂存当前工作目录中的更改
 git stash save "Temporarily stashed changes"
@@ -13,19 +23,19 @@ git stash save "Temporarily stashed changes"
 for COMMIT in $COMMITS; do
     # 回滚到指定的提交
     git checkout $COMMIT
-   
+
     # 修改提交的作者信息
     git commit --amend --no-edit --author="Your Name <$NEW_EMAIL>"
-   
+
     # 打印出修改的提交
     echo "Modified commit $COMMIT"
 done
 
-# 回到 HEAD，即最新的提交
-# git checkout main
+# 切换回当前分支
+git checkout $CURRENT_BRANCH
 
 # 恢复暂存的更改
 git stash pop
 
 # 强制推送所有修改过的提交
-git push origin main --force
+git push origin $CURRENT_BRANCH --force
