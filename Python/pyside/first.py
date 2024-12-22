0752a8f15a52d93c231a7b20c73a35236efd0262
cyd1st <43594415+cyd1st@users.noreply.github.com>
2024-12-23 01:15:21 +0800

    sync.

--- Python/pyside/first.py
+++ Python/pyside/first.py
@@ -1,21 +1,27 @@
-import PySide2
+import sys
+from PySide6.QtCore import QObject, Qt
+from PySide6.QtWidgets import QApplication, QLineEdit, QPushButton, QVBoxLayout, QWidget
 
-class MyForm(PySide2.QtCore.QObject):
-    def __init__(self):
+class MyForm(QObject):
+    def __init__(self, app):
         super().__init__()
 
-        self.text_input = PySide2.QtWidgets.QLineEdit()
-        self.button = PySide2.QtWidgets.QPushButton("Submit")
+        self.text_input = QLineEdit()
+        self.button = QPushButton("Submit")
 
-        self.layout = PySide2.QtWidgets.QVBoxLayout()
+        self.layout = QVBoxLayout()
         self.layout.addWidget(self.text_input)
         self.layout.addWidget(self.button)
 
-        self.widget = PySide2.QtWidgets.QWidget()
+        self.widget = QWidget()
         self.widget.setLayout(self.layout)
 
     def show(self):
         self.widget.show()
 
-form = MyForm()
-form.show()
+# 创建并显示表单
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = MyForm(app)
+    form.show()
+    sys.exit(app.exec())
